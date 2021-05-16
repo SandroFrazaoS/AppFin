e04eaa6085156a1c18830881520fd89636a703df
Sandro Frazao Specht <sandro.s.frazao@gmail.com>
2021-05-16 19:36:12 -0300

    correção

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -4,14 +4,7 @@ import 'firebase/database';
 
 
 let firebaseConfig = {
-    apiKey: "AIzaSyC4y2OIsJ02FsIyUtBwxcPmTtoEm1CnFfU",
-    authDomain: "meuapp-632cf.firebaseapp.com",
-    databaseURL: "https://meuapp-632cf-default-rtdb.firebaseio.com",
-    projectId: "meuapp-632cf",
-    storageBucket: "meuapp-632cf.appspot.com",
-    messagingSenderId: "694504782541",
-    appId: "1:694504782541:web:67460882a8d1093e9c8784",
-    measurementId: "G-0B8MKX2XD0"
+
   };
   
   // Initialize Firebase
